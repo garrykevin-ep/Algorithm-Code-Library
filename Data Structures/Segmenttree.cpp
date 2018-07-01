4c3dc26e8d7824fab0f9b2c28505cf4251cd6958
garrykevin <garrykevin.ep@gmail.com>
2018-07-01 16:45:19 +0530

    seg tree

--- Data Structures/Segmenttree.cpp
+++ Data Structures/Segmenttree.cpp
@@ -4,67 +4,53 @@ using namespace std;
 #define foreach(i,n) for(__typeof((n).begin())i =(n).begin();i!=(n).end();i++)
 #define rep(i,n) for(__typeof(n) i=0; i<(n); i++)
 #define mem(x,val) memset((x),(val),sizeof(x));
-#define all(x) x.begin(),x.end()
-#define pb push_back
-#define mp make_pair
-#define ins insert
-#define ff first
-#define ss second
-#define ll long long
-typedef string st;
-typedef vector<int> vi;
-typedef vector<st> vs;
-typedef map<int,int> mii;
-typedef map<st,int> msi;
-typedef set<int> si;
-typedef set<st> ss;
-typedef pair<int,int> pii;
-typedef vector<pii> vpii;
 
 #define debug1(x) cout << #x << ": " << x << endl
 #define debug2(x, y) cout << #x << ": " << x << '\t' << #y << ": " << y << endl
 #define debug3(x, y, z) cout << #x << ": " << x << '\t' << #y << ": " << y << '\t' << #z << ": " << z << endl
 
+int a[5]={1,2,3,4,5},tree[10];
+
+int build(int start,int end,int node)
+{
+  if(start == end)
+  {
+    tree[node] = a[end];
+    return tree[node];
+  }
+  else
+  {
+    int mid = (start + end)/2;
+    tree[node] = build(start,mid,2*node+1) + build(mid+1,end,(2*node)+2);
+    return tree[node];
+  }
+}
+
+int query(int start ,int end , int l ,int r,int node)
+{
+  
+  //totally outside
+  if(r < start  || end < l)
+    return 0;
+  //totally inside
+  if(l <= start && end <= r)
+    return tree[node];
+  
+  //partial inside
+
+  int mid = (start+end)/2;
+  return query(start,mid,l,r,2*node+1) + query(mid+1,end,l,r,2*node+2);
+}
 
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        int am=0,bm=0,asum=0,bsum=0;
-        int te;
-        cin>>n;
-        rep(i,n)
-        {
-            cin>>te;
-            asum += te;
-            if(te > am)
-                am = te;
-        }
+  build(0,4,0);
+  // rep(i,10)debug1(tree[i]);
+ 
+  // cout<<query(0,4,0,4,0)<<endl;
+  cout<<query(0,4,0,2,0)<<endl;
+  // cout<<query(0,4,0,3,0)<<endl;
+  cout<<query(0,4,1,3,0)<<endl;
 
-        asum -=am;
-
-        rep(i,n)
-        {
-            cin>>te;
-            bsum += te;
-            if(te > bm)
-                bm = te;
-        }
-
-        bsum -=bm;
-
-        if(bsum == asum)
-            cout<<"Draw\n";
-        else if(asum < bsum)
-            cout<<"Alice\n";
-        else
-            cout<<"Bob\"
-
-
-    }
-
-    return 0;
+  return 0;
 }
